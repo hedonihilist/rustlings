8fa242ea7811c44faf8b64072b2420091456769e
Hedonihilist <zenghao_mail@163.com>
2021-09-16 12:45:53 +0800

    some solutions

--- exercises/error_handling/errors5.rs
+++ exercises/error_handling/errors5.rs
@@ -4,14 +4,12 @@
 // It won't compile right now! Why?
 // Execute `rustlings hint errors5` for hints!
 
-// I AM NOT DONE
-
 use std::error;
 use std::fmt;
 use std::num::ParseIntError;
 
 // TODO: update the return type of `main()` to make this compile.
-fn main() -> Result<(), ParseIntError> {
+fn main() -> Result<(), Box<dyn error::Error>> {
     let pretend_user_input = "42";
     let x: i64 = pretend_user_input.parse()?;
     println!("output={:?}", PositiveNonzeroInteger::new(x)?);
@@ -38,6 +36,7 @@ impl PositiveNonzeroInteger {
         }
     }
 }
+
 
 // This is required so that `CreationError` can implement `error::Error`.
 impl fmt::Display for CreationError {
--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,7 +1,9 @@
 // functions1.rs
 // Make me compile! Execute `rustlings hint functions1` for hints :)
 
-// I AM NOT DONE
+fn call_me() {
+    println!("Who is there?");
+}
 
 fn main() {
     call_me();
